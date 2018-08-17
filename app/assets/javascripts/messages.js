4625d3caf09f9a163d4b30dd96741c2c3bfd04c3
Owner-PC\User <ooba.nori@gmail.com>
2018-08-17 16:25:47 +0900

    Create HTML and fails alert
    帰ってきたjsonをdoneメソッドで受け取りHTMLを作成
    作成したHTMLを一番下に追加
    非同期に失敗した場合のエラー処理の設定

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,17 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<div class="chat__message-name">
+                  ${message.user_name}
+                </div>
+                <div calss="chat__message-time">
+                  ${message.created_at}
+                </div>
+                <div class="chat__message-body">
+                  ${message.body}
+                </div>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +24,13 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $(".chat-body__messages-list").append(html);
+      $(".chat-footer__form__message").val('');
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
-})
+});
