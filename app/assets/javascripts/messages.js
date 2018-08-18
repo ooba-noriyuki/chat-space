68988e0b44c601fa947d1469a97b8ba567be6c4d
Owner-PC\User <ooba.nori@gmail.com>
2018-08-18 14:45:01 +0900

    edit messages.js and create.json.jbuilder
    messages.jsのhtmlの作成

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,33 +1,43 @@
 $(function(){
   function buildHTML(message){
+    var img = message.image? `<img src=${message.image}>` : "";
     var html = `<div class="chat__message-name">
                   ${message.user_name}
                 </div>
-                <div calss="chat__message-time">
+                <div class="chat__message-time">
                   ${message.created_at}
                 </div>
                 <div class="chat__message-body">
                   ${message.body}
+                </div>
+                <div class="chat__message-body">
+                  ${img}
                 </div>`
     return html;
+  }
+
+  function scroll(messagebox){
+    messagebox.animate({scrollTop: $('.chat-body__messages-list')[0].scrollHeight},'fast');
   }
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
       data: formData,
-      datatype: 'json',
+      dataType: 'json',
       processData: false,
       contentType: false
     })
     .done(function(data){
       var html = buildHTML(data);
-      $(".chat-body__messages-list").append(html);
-      $(".chat-footer__form__message").val('');
+      $('.chat-body__messages-list').append(html);
+      $('#new_message')[0].reset();
+      $('.chat-footer__form__send-btn').prop('disabled', false);
+      scroll($('.chat-body__messages-list'));
     })
     .fail(function(){
       alert('error');
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,4 @@
 json.body @message.body
 json.image @message.image
 json.created_at @message.created_at
-json.user_id @message.user.id
 json.user_name @message.user.name
